22a6e0af4648e51ad76ea239db3f163b785d06bd
Jérémie Bouchard <bouj0601@usherbrooke.ca>
2025-07-18 23:37:20 -0400

    restart btn + btn hover color + removed unused assets

--- Assets/Scripts/Widgets/WinStateWidget.cs
+++ Assets/Scripts/Widgets/WinStateWidget.cs
@@ -1,16 +1,30 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace DefaultNamespace.Widgets
 {
     public class WinStateWidget : Widget
     {
+        public Button restartButton;
         public TextMeshProUGUI winStateText;
+
+        protected void Awake()
+        {
+            restartButton.onClick.AddListener(Restart);
+        }
 
         protected override void Start()
         {
             base.Start();
             _isVisible = false;
             gameObject.SetActive(false);
+        }
+
+        private void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
